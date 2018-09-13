db13ea1d912eaf56045eb1e05d7a499d0f4b0cfd
PavloShvoruk <duckdogers07@gmail.com>
2018-09-13 19:48:47 +0300

    turnString done

--- src/turnMeBaby/index.js
+++ src/turnMeBaby/index.js
@@ -9,6 +9,10 @@
  */
 const turnMeBaby = str => {
   /* your logic here...*/
+  let splitString = str.split('');
+  let reverseArray = splitString.reverse();
+  let joinArray = reverseArray.join('');
+  return joinArray;
 };
 
 export default turnMeBaby;
